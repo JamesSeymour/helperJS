555be31e7ed27226451816c9f8d61892b0192c40
James Seymour <seymonster@gmail.com>
2013-06-11 23:33:11 +0100

    First commit of HelperJS

---
+++ HelperJS.js
@@ -0,0 +1,87 @@
+HelperJS = function(sIdentifier) {
+	this.bIsOverlayOpen = false;
+
+	var self = this;
+	$("#" + sIdentifier).bind('click', function() {
+		self.addOverlay();
+	});
+
+	$(window).resize(function() {
+ 		if(self.bIsOverlayOpen)
+ 		{
+ 			self.removeOverlay();
+ 			self.addOverlay();
+ 		}
+	});
+};
+
+HelperJS.prototype.addOverlay = function() {
+	var eHelpElement = $(document).find('[helper-bind]');
+	this.bIsOverlayOpen = true;
+
+	this.eLeftElement = this._createLeftElement(eHelpElement);
+	this.eTopElement = this._createTopElement(eHelpElement);
+	this.eRightElement = this._createRightElement(eHelpElement);
+	this.eBottomElement = this._createBottomElement(eHelpElement);
+};
+
+HelperJS.prototype.removeOverlay = function() {
+	this.bIsOverlayOpen = false;
+	this.eLeftElement.remove();
+	this.eTopElement.remove();
+	this.eRightElement.remove();
+	this.eBottomElement.remove();
+};
+
+HelperJS.prototype._createLeftElement = function(eHelpElement) {
+	var nDistanceFromLeft = eHelpElement.position().left;
+	var nWindowHeight = $(window).height();
+
+	return this._createOverlayElement(nWindowHeight, nDistanceFromLeft, 0, 0);
+};
+
+HelperJS.prototype._createTopElement = function(eHelpElement) {
+	var nDistanceFromTop = eHelpElement.position().top;
+	var nDistanceFromLeft = eHelpElement.position().left;
+
+	return this._createOverlayElement(nDistanceFromTop, eHelpElement.width(), 0, nDistanceFromLeft);
+};
+
+HelperJS.prototype._createRightElement = function(eHelpElement) {
+	var nDistanceFromTop = eHelpElement.position().top;
+	var nDistanceFromLeft = eHelpElement.position().left;
+	var nWindowWidth = $(window).width();
+	var nWindowHeight = $(window).height();
+
+	var nRightElementWidth = nWindowWidth - (nDistanceFromLeft + eHelpElement.width())
+	var nLeftOffset = nDistanceFromLeft + eHelpElement.width();
+
+	return this._createOverlayElement(nWindowHeight, nRightElementWidth, 0, nLeftOffset);
+};
+
+HelperJS.prototype._createBottomElement = function(eHelpElement) {
+	var nDistanceFromTop = eHelpElement.position().top;
+	var nDistanceFromLeft = eHelpElement.position().left;
+	var nWindowHeight = $(window).height();
+
+	var nDistanceFromBottom = nDistanceFromTop + eHelpElement.height();
+	var nTopElementHight = nWindowHeight - nDistanceFromBottom;
+
+	return this._createOverlayElement(nTopElementHight, eHelpElement.width(), nDistanceFromBottom, nDistanceFromLeft);
+};
+
+HelperJS.prototype._createOverlayElement = function(nHeight, nWidth, nTop, nLeft) {
+	var eElement = $("<div class='overlay-body'></div>");
+	eElement.css("height", nHeight + "px");
+	eElement.css("width", nWidth + "px");
+	eElement.offset({ top: nTop, left: nLeft });
+
+	$("body").append(eElement);
+
+	var self = this;
+	eElement.bind('click', function() {
+		self.removeOverlay();
+	});
+
+	return eElement;
+};
